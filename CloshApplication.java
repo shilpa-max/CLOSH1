455ca9ca8f173bcaec2663c851c9b3824cebf06f
shilpa-max <99542354+shilpa-max@users.noreply.github.com>
2022-04-19 17:00:43 +0530

    Add files via upload

---
+++ CloshApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.closh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloshApplication.class, args);
+	}
+
+}
